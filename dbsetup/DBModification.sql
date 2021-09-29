91477b3046f08d559aa36af60f0ea96f02a13d7b
aw11hjia <jianha9@bu.edu>
2021-09-28 20:30:37 -0400

    ln

--- dbsetup/DBModification.sql
+++ dbsetup/DBModification.sql
@@ -5,3 +5,6 @@ MODIFY COLUMN classSchedule varchar(8);
 use registration;
 ALTER TABLE class
 ADD COLUMN title varchar(50);
+
+alter table student drop column lastLame;
+alter table student add lastName varchar(15);
